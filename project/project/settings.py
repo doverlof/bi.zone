9ad9160cbc2566493f09ab92d41673d00a19624e
doverlof <doverlof@gmail.com>
2024-11-11 00:25:47 +0300

    collect_project

--- project/project/settings.py
+++ project/project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -77,12 +78,8 @@ WSGI_APPLICATION = "project.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "IP",  # Имя базы данных
-        "USER": "postgres",  # Имя пользователя
-        "PASSWORD": "tosmare",  # Пароль
-        "HOST": "localhost",  # Хост
-        "PORT": "1234",  # Порт
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
 
