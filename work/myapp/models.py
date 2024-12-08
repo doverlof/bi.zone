3c422f8344cd8b57a482acfab17498f5a1a6769b
filinovdg <filinovdg@student.bmstu.ru>
2024-12-08 21:07:13 +0300

    only 1 ip and test

--- work/myapp/models.py
+++ work/myapp/models.py
@@ -4,7 +4,7 @@ import ipaddress
 
 
 class Subnet(models.Model):
-    subnet = models.CharField(max_length=18)
+    subnet = models.CharField(max_length=18, unique=True)
     company_id = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
 
